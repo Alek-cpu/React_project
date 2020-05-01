8ea7e0f443b837e06860065b259cec257eca07b7
Aleksandr Pryanikov <a.pryanikov@truemachine.ru>
2020-05-02 00:13:40 +0400

    preLast day

--- src/components/Dashboard/cards/Cards.jsx
+++ src/components/Dashboard/cards/Cards.jsx
@@ -3,42 +3,31 @@ import Card from "../card/Card";
 import './Cards.css';
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
-// fake data generator
 const getItems = count =>
     Array.from({ length: count }, (v, k) => k).map(k => ({
         id: `item-${k}`,
         content: `item ${k}`
     }));
 
-
-
-
-// a little function to help us with reordering the result
 const reorder = (list, startIndex, endIndex) => {
     const result = Array.from(list);
     const [removed] = result.splice(startIndex, 1);
     result.splice(endIndex, 0, removed);
-
     return result;
 };
 
 const grid = 8;
 
 const getItemStyle = (isDragging, draggableStyle) => ({
-    // some basic styles to make the items look a bit nicer
     userSelect: "none",
     padding: grid * 2,
     margin: `0 0 ${grid}px 0`,
 
-    // change background colour if dragging
     background: isDragging ? "lightgreen" : "grey",
-
-    // styles we need to apply on draggables
     ...draggableStyle
 });
 
 const getListStyle = isDraggingOver => ({
-    // background: isDraggingOver ? "lightblue" : "lightgrey",
     padding: grid,
     width: `${100}%`
 });
@@ -52,16 +41,7 @@ class Cards extends Component {
         this.onDragEnd = this.onDragEnd.bind(this);
     }
 
-    // let cardLines =
-    //     props.cardInfo.map( c => (<Card eventName={c.eventName}
-    //                                     dueDate={c.dueDate}
-    //                                     userName={c.userName}
-    //                                     identicalStatus={c.identicalStatus}
-    //                                     statusName={c.statusName}
-    //     />));
-
     onDragEnd(result) {
-        // dropped outside the list
         if (!result.destination) {
             return;
         }
@@ -76,19 +56,6 @@ class Cards extends Component {
             items
         });
     }
-    // let cardLines =
-    //     props.cardInfo.map( c => (<Card eventName={c.eventName}
-    //                                  dueDate={c.dueDate}
-    //                                  userName={c.userName}
-    //                                  identicalStatus={c.identicalStatus}
-    //                                  statusName={c.statusName}
-    //     />));
-
-    // return (
-    //     <div className="drag-drop-wrapper">
-    //         {cardLines}
-    //     </div>
-    // );
     render() {
         return (
             <DragDropContext onDragEnd={this.onDragEnd}>
