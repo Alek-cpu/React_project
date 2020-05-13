41daee796baf62a06b21fa9674545a09dceff735
Aleksandr Pryanikov <a.pryanikov@truemachine.ru>
2020-05-13 20:41:37 +0400

    refactor

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from './App';
-import * as serviceWorker from "./serviceWorker";
 import {Provider} from "react-redux";
 import {createStore} from "redux";
 import allReducers from "./reducers/index";
@@ -17,5 +16,3 @@ ReactDOM.render(
     </React.StrictMode>,
     document.getElementById('root')
 );
-
-serviceWorker.unregister();
